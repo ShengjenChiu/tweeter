bbc523d83d378ed80eb9d690485050b0633a3378
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-07-02 10:15:33 -0600

    composer-char-counter.js: update it with commenet and new if-condition

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,4 @@
+//The function handles the counter digits appearence on the DOM UI in real time
 $(document).ready(function () {
   $("#tweet-text").on("input", function () {
     let charLength = $(this).val().length;
@@ -6,6 +7,10 @@ $(document).ready(function () {
   
     if (charCounter <= 0) {
       $counter.text(charCounter).css("color", "red");
+    } 
+
+    if (!$(this).val()) {
+      $counter.text(charCounter).css("color", "black");
     }
 
     $counter.text(charCounter);
